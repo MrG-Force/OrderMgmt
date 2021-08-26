07f20ae305e7c70088c6236e266e60d0bde57e98
MrGForce <willortizloza@hotmail.com>
2021-08-26 16:11:18 +1000

    Add User Interface(Views) that tagets .Net 5.0

---
+++ OrderMgmtSystem/OrderMgmtGUI/Navigation/Switcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OrderMgmtGUI.Navigation
+{
+    public class Switcher
+    {
+        public static MainWindow mainWindow;
+
+        public static void Switch(UserControl newPage)
+        {
+            //mainWindow.Navigate(newPage);
+        }
+    }
+}
